8f530f18cd9f9933d1e89f8bbd499397032c613b
Armaningtyas Utami <armaningtyas_utami_24rpl@student.smktelkom-mlg.sch.id>
2017-02-07 13:32:52 +0700

    Asp_mvc_2

--- Asp_mvc_2/Controllers/HomeController.cs
+++ Asp_mvc_2/Controllers/HomeController.cs
@@ -1,15 +1,27 @@
-﻿using System.Web.Mvc;
+﻿using Asp_mvc_2.Security;
+using System.Web.Mvc;
 
 namespace Asp_mvc_2.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index() {
+        public ActionResult Index()
+        {
             return View();
         }
 
         [Authorize]
         public ActionResult Welcome()
+        {
+            return View();
+        }
+
+        [AuthorizeRoles("Admin")]
+        public ActionResult AdminOnly()
+        {
+            return View();
+        }
+        public ActionResult UnAuthorized()
         {
             return View();
         }
